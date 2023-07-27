48fd879c1130a129ca9a8d0e57b7f95a2e129d9b
sujang.958 <enter7377@naver.com>
2023-07-27 11:08:15 +0900

    ref: component PlayerCard

--- frontend/src/routes/live/[id]/+page.svelte
+++ frontend/src/routes/live/[id]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from "$app/stores"
 	import Board from "$lib/Board.svelte"
+	import PlayerCard from "$lib/PlayerCard.svelte"
 	import { getSquare } from "$lib/board"
 	import { toReversed } from "$lib/utils"
 	import { Chess } from "chess.js"
@@ -161,49 +162,13 @@
 	/>
 
 	<div class="bg-neutral-900 rounded-xl w-1/6 p-4 flex flex-col justify-between h-96">
-		<div class="flex flex-col gap-y-4">
-			<button class="rounded-xl bg-neutral-800 text-center font-semibold py-4 text-2xl"
-				>2 | 1</button
-			>
-			<button
-				class="rounded-xl bg-neutral-200 text-neutral-800 text-center font-semibold py-2.5 text-2xl"
-				>PLAY</button
-			>
+		<PlayerCard />
+		<div class="grid grid-cols-2 gap-2">
+			{#each history as move}
+					{move}
+			{/each}
 		</div>
-		<div class="flex flex-row items-center justify-between pb-1">
-			<div class="flex flex-row items-center gap-x-3">
-				<img
-					src="/popcat-with-bg.jpg"
-					alt="pfp"
-					class="rounded-lg w-8 h-8 object-contain"
-					draggable="false"
-				/>
-				<p class="font-medium text-lg">
-					Hikaru Nakamura &nbsp;<span class="text-neutral-400 text-xs">(6900)</span>
-				</p>
-			</div>
-			<button type="button">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-5 h-5 stroke-neutral-400"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-					/>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-					/>
-				</svg>
-			</button>
-		</div>
+		<PlayerCard settingsButtonVisible />
 	</div>
 </div>
 
