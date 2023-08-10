296fbd2656c82b2c7c88a8b7fab0f7efb9b76727
sujang.958 <enter7377@naver.com>
2023-08-10 18:56:59 +0900

    feat: limit the length of names

--- backend/src/gql/mutations/changeName.ts
+++ backend/src/gql/mutations/changeName.ts
@@ -1,19 +1,22 @@
 import prisma from "../../../prisma/prisma.js"
 import { userAction } from "../actions.js"
 
-export const changeName = userAction(
-  async (user, { name }) =>
-    await prisma.user.update({
-      where: {
-        id: user.id,
-      },
-      data: {
-        name,
-      },
-      include: {
-        blackGames: true,
-        whiteGames: true,
-        wonGames: true,
-      },
-    })
-)
+export const changeName = userAction(async (user, { name: _name }) => {
+  const name = String(_name).trim()
+
+  if (name.length > 50) return null
+
+  return await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      name,
+    },
+    include: {
+      blackGames: true,
+      whiteGames: true,
+      wonGames: true,
+    },
+  })
+})
