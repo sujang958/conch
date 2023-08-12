740e71185207b8a047798f15a946be6e6f429681
Songhun Cho <enter7377@naver.com>
2023-08-12 04:07:48 +0000

    feat: add 'games' in GraphQL

--- backend/src/gql/index.ts
+++ backend/src/gql/index.ts
@@ -14,6 +14,7 @@ import {
   fastifyApolloHandler,
 } from "@as-integrations/fastify"
 import { GraphQLResolveInfo } from "graphql"
+import { games } from "./queries/games.js"
 
 export type Context = {
   req: FastifyRequest
@@ -48,6 +49,7 @@ const resolvers: Resolvers = {
         )
       )
     },
+    games,
   },
   Mutation: {
     login,
--- backend/src/gql/queries/games.ts
+++ backend/src/gql/queries/games.ts
@@ -1,1 +1,17 @@
-export const games = publicAction
+import prisma from "../../../prisma/prisma.js"
+import { publicAction } from "../actions.js"
+
+export const games = publicAction(async ({ userId }) => {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+    include: { blackGames: true, wonGames: true, whiteGames: true },
+  })
+
+  if (!user) return
+
+  return {
+    wonGames: user.wonGames,
+    blackGames: user.blackGames,
+    whiteGames: user.whiteGames,
+  }
+})
